ec88421528081395e0a39ee01ddc2e3cb1759b6c
youngbloood <944168348@qq.com>
2023-12-06 16:58:30 +0800

    delete: cmd/target

--- cmd/target/debug/build/anyhow-68feb3bd9647985a/build_script_build-68feb3bd9647985a.d
+++
@@ -1,5 +0,0 @@
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/build/anyhow-68feb3bd9647985a/build_script_build-68feb3bd9647985a: /Users/jerad.k/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/anyhow-1.0.75/build.rs
-
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/build/anyhow-68feb3bd9647985a/build_script_build-68feb3bd9647985a.d: /Users/jerad.k/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/anyhow-1.0.75/build.rs
-
-/Users/jerad.k/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/anyhow-1.0.75/build.rs:
--- cmd/target/debug/deps/rsbit-e7c9cea20d54f812.d
+++
@@ -1,7 +0,0 @@
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/deps/librsbit-e7c9cea20d54f812.rmeta: /Users/jerad.k/.cargo/git/checkouts/rsbit-24472aecc3de0ee5/e23268e/src/lib.rs
-
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/deps/librsbit-e7c9cea20d54f812.rlib: /Users/jerad.k/.cargo/git/checkouts/rsbit-24472aecc3de0ee5/e23268e/src/lib.rs
-
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/deps/rsbit-e7c9cea20d54f812.d: /Users/jerad.k/.cargo/git/checkouts/rsbit-24472aecc3de0ee5/e23268e/src/lib.rs
-
-/Users/jerad.k/.cargo/git/checkouts/rsbit-24472aecc3de0ee5/e23268e/src/lib.rs:
