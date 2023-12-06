ad1fc58ef9db08084386275b6a806e3b6527ef9b
youngbloood <944168348@qq.com>
2023-12-06 16:56:48 +0800

    feat: zone of NameServer

--- cmd/target/debug/deps/cmd-22fa0fd773d02506.d
+++
@@ -1,5 +0,0 @@
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/deps/libcmd-22fa0fd773d02506.rmeta: src/main.rs
-
-/Users/jerad.k/code/rust/github.com/youngbloood/rsdns/cmd/target/debug/deps/cmd-22fa0fd773d02506.d: src/main.rs
-
-src/main.rs:
