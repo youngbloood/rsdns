85e9cc92d518805926f3cce85c6239e6015e5dc1
youngbloood <944168348@qq.com>
2023-12-15 18:47:43 +0800

    feat(rdata): impl cname

--- src/dns/rdata/cname.rs
+++ src/dns/rdata/cname.rs
@@ -1,3 +1,7 @@
+use anyhow::Error;
+
+use super::RDataOperation;
+
 /*！
 ref: https://www.rfc-editor.org/rfc/rfc1035#section-3.3.1
 
@@ -16,4 +20,24 @@ CNAME RRs cause no additional section processing, but name servers may
 choose to restart the query at the canonical name in certain cases.  See
 the description of name server logic in [RFC-1034] for details.
 */
+
+#[derive(Debug)]
 pub struct CNAME(String);
+
+impl CNAME {
+    pub fn from(raw: &[u8]) -> Result<Self, Error> {
+        Ok(CNAME {
+            0: String::from_utf8(raw.to_vec())?,
+        })
+    }
+}
+
+impl RDataOperation for CNAME {
+    fn decode(&self) -> Vec<String> {
+        return vec![self.0.clone()];
+    }
+
+    fn encode(&self) -> Vec<u8> {
+        return self.0.clone().into_bytes();
+    }
+}
--- src/dns/rdata/mod.rs
+++ src/dns/rdata/mod.rs
@@ -4,7 +4,7 @@ use std::fmt::Debug;
 use anyhow::Error;
 pub use cname::CNAME;
 
-use super::Type;
+use super::{Type, TYPE_A, TYPE_CNAME};
 
 pub trait RDataOperation: Debug {
     fn decode(&self) -> Vec<String>;
@@ -30,10 +30,23 @@ impl RData {
     }
 
     pub fn from(raw: Vec<u8>, typ: Type) -> Result<Self, Error> {
-        Ok(Self {
+        let mut rdata = Self {
             raw,
             resource: todo!(),
-        })
+        };
+
+        match typ {
+            TYPE_CNAME => {
+                let cname = CNAME::from(&raw.to_vec());
+                if cname.is_err() {
+                    return Err(cname.err().unwrap());
+                }
+                rdata.resource = Box::new(cname.unwrap());
+            }
+            _ => todo!(),
+        }
+
+        Err(Error::msg("todo"))
     }
 
     pub fn encode(&self) -> Vec<u8> {
